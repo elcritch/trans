5a98cbcdb4610498e8c244c301f299bfd673a743
Jaremy Creechley <creechley@gmail.com>
2009-03-03 00:52:47 -0700

    generate parser produces a lot of the code, still needs few corner cases

--- generate_parser.rb
+++ generate_parser.rb
@@ -4,96 +4,106 @@ $sp = "   "
 $sp2 = $sp*2
 
 def term?(tok)
-  return tok =~ /^'.+'$/
+   return tok =~ /^'.+'$/
 end
 
 def rule?(tok)
-  return !sym?(tok)
+   return !sym?(tok)
 end
 
 def sym(tok)
-  regex = Regexp.new(/^'(.*)'$/)
-  match = regex.match(tok)
-  return match[1]
+   regex = Regexp.new(/^'(.*)'$/)
+   match = regex.match(tok)
+   return match ? match[1] : tok
 end
 
 def eater(tok)
-  str = ""
-  if term?(tok)
-    tk = sym(tok)
-    if tk.length == 1
-      str = "eat('#{tk}')" 
-    else 
-      str = "eat(TOK_#{tk})"
-    end
-  else
-    str = "Tree#{tok.capitalize} = p_#{tok}()"
-  end
-  return str
+   str = ""
+   if term?(tok)
+      tk = sym(tok)
+      if tk.length == 1
+         str = "eat('#{tk}')" 
+      else 
+         str = "eat(TOK_#{tk})"
+      end
+   else
+      str = "Tree#{tok.capitalize} = p_#{tok}()"
+   end
+   return str
 end
 
 def caser(rule, prod)
-  toks = prod.collect { |p| eater(p) }
-  args = prod.collect { |p| p if p =~ /^\w+/ }
-  return <<-CASE
-   case TOK_#{prod.first}: {
-      #{ toks*";\n#{$sp2}" };
-      #{rule} = t_#{rule}_#{prod.first}(#{ args*', ' })
-      break;
-    }
-  CASE
+   toks = prod.collect { |p| eater(p) }
+   args = prod.collect { |p| p if p =~ /^\w+/ }
+   cas = "TOK_#{sym(prod.first)}"
+   cas = "#{prod.first}" if sym(prod.first).length == 1
+   
+   return """      case #{cas}: {
+         #{ toks*";\n#{$sp*3}" };
+         #{rule} = t_#{rule}_#{sym(prod.first)}(#{ args*', ' });
+         break;
+      }"""
 end
 
 def functer(rule, prods)
-  err_parse = prods.last.empty? ? "" : "#{$sp*3}error_parse(\"#{rule}\");\n"
-  default = "#{$sp}default:\n#{err_parse}#{$sp2}break;"
-  if prods.length > 1
-    body = <<-BODY
-  switch (code) {
+   err_parse = prods.last.empty? ? "" : "#{$sp*3}error_parse(\"#{rule}\");\n"
+   default = "#{$sp2}default:\n#{err_parse}#{$sp*3}break;"
+   if prods.length > 1
+      body = <<-BODY
+   
+   // cases
+   switch (code) {
 #{ prods.collect {|p| caser(rule, p) if not p.empty? }*"\n" }
 #{default}
-  }  
-  BODY
-  else
-    body = <<-BODY
-    BODY
-  end
-  
-  return <<-FUNC
+   }   
+   BODY
+   elsif prods.length == 1
+      toks = prods.first.collect { |p| eater(p) }
+      args = prods.first.collect { |p| p if p =~ /^\w+/ }
+      
+      body = <<-BODY
+   
+   // body
+   #{ toks*";\n#{$sp}" };
+   #{rule} = t_#{rule}_#{prods.first.first}(#{ args*', ' });
+   BODY
+   end
+   
+   return <<-FUNC
 /** 
-  #{rule.capitalize}
-  Grammar:
-    #{rule} : #{prods.collect { |p| p*' '}*" | "} #{'e'if prods.last.empty?}
+   #{rule.capitalize}
+   Grammar:
+      #{rule} : #{prods.collect { |p| p*' '}*" | "} #{'e'if prods.last.empty?}
 */
 static Tree#{rule.capitalize} p_#{rule}(void) {
-  Tree#{rule.capitalize} = 0; // set null by default
-  TokenCode code = curr()->code;
+   Tree#{rule.capitalize} = 0; // set null by default
+   TokenCode code = curr()->code;
 #{body}
-  return stmt;
+   return stmt;
 }
 
 
 
-  FUNC
+   FUNC
 end
 
 
 File.open("grammar-right.txt", "r") do |fl|
-  $/="\n\n"
-  coll = fl.collect {|g| g.split(':') }
+   $/="\n\n"
+   coll = fl.collect {|g| g.split(':') }
 
-  coll.each do |prod| 
-    # puts "LINE: '#{prod.inspect}'"
-    rule = prod.shift.strip
-    prods = prod.last ? prod.last.split('|').collect{ |p| p.split } : []
-    # puts "rule: #{rule.inspect}"
-    # puts "prods:\t #{prods.inspect}"
-    # puts
-    if not rule.empty?
-      grammar[rule] = prods 
-      gram << [rule, prods]
-    end
-  end
+   coll.each do |prod| 
+      # puts "LINE: '#{prod.inspect}'"
+      rule = prod.shift.strip
+      prods = prod.last ? prod.last.split(/\s+\|\s+/).collect{ |p| p.split } : []
+      # puts "rule: #{rule.inspect}"
+      # puts "prods:\t #{prods.inspect}"
+      # puts
+      if not rule.empty?
+         grammar[rule] = prods 
+         gram << [rule, prods]
+      end
+   end
 
 end
 
