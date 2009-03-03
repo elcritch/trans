d67852b6aacad1f95d4085af161960cfa9647862
Jaremy Creechley <creechley@gmail.com>
2009-03-03 00:24:58 -0700

    fixing no case body

--- generate_parser.rb
+++ generate_parser.rb
@@ -3,20 +3,31 @@ gram = []
 $sp = "   "
 $sp2 = $sp*2
 
+def term?(tok)
+  return tok =~ /^'.+'$/
+end
+
+def rule?(tok)
+  return !sym?(tok)
+end
+
+def sym(tok)
+  regex = Regexp.new(/^'(.*)'$/)
+  match = regex.match(tok)
+  return match[1]
+end
+
 def eater(tok)
   str = ""
-  regex = Regexp.new(/'(.+)'/)
-  match = regex.match(tok)
-  if match
-    tk = match[0]
+  if term?(tok)
+    tk = sym(tok)
     if tk.length == 1
       str = "eat('#{tk}')" 
     else 
       str = "eat(TOK_#{tk})"
     end
-    
   else
-    str = "Tree#{tok.capitalize} = p_#{tok}"
+    str = "Tree#{tok.capitalize} = p_#{tok}()"
   end
   return str
 end
@@ -26,7 +37,7 @@ def caser(rule, prod)
   args = prod.collect { |p| p if p =~ /^\w+/ }
   return <<-CASE
    case TOK_#{prod.first}: {
-      #{ toks*";\n#{$sp2}" }
+      #{ toks*";\n#{$sp2}" };
       #{rule} = t_#{rule}_#{prod.first}(#{ args*', ' })
       break;
     }
@@ -34,21 +45,30 @@ def caser(rule, prod)
 end
 
 def functer(rule, prods)
-  if prods.last.empty? 
-    default = "default:\n#{$sp2}break;"
+  err_parse = prods.last.empty? ? "" : "#{$sp*3}error_parse(\"#{rule}\");\n"
+  default = "#{$sp}default:\n#{err_parse}#{$sp2}break;"
+  if prods.length > 1
+    body = <<-BODY
+  switch (code) {
+#{ prods.collect {|p| caser(rule, p) if not p.empty? }*"\n" }
+#{default}
+  }  
+  BODY
   else
-    default = "default:\n#{$sp2}error_parse(\"#{rule}\");\n#{$sp2}break;"
+    body = <<-BODY
+    BODY
   end
+  
   return <<-FUNC
-/** #{rule} */
+/** 
+  #{rule.capitalize}
+  Grammar:
+    #{rule} : #{prods.collect { |p| p*' '}*" | "} #{'e'if prods.last.empty?}
+*/
 static Tree#{rule.capitalize} p_#{rule}(void) {
   Tree#{rule.capitalize} = 0; // set null by default
   TokenCode code = curr()->code;
-  switch (code) {
-#{ prods.collect {|p| caser(rule, p) }*"\n" }
-    #{default};
-    break;
-  }
+#{body}
   return stmt;
 }
 
@@ -69,15 +89,16 @@ File.open("grammar-right.txt", "r") do |fl|
     # puts "rule: #{rule.inspect}"
     # puts "prods:\t #{prods.inspect}"
     # puts
-    grammar[rule] = prods if not rule.empty?
-    gram << [rule, prods]
+    if not rule.empty?
+      grammar[rule] = prods 
+      gram << [rule, prods]
+    end
   end
 
 end
 
 # puts "G1", grammar["factor"].inspect
-# puts "Grammar: "
-
+puts "Grammar: "
 gram.each { |g| puts functer(g.first, g.last) }
 
 
